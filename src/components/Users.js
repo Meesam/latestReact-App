81b508df6480d494dc395d74ee50ed9b809e69c8
Meesam Z <13222@IIPL.COM>
2018-01-29 13:05:54 +0530

    enable google authentication

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,32 @@
 import React,{Component} from 'react';
 
 class Users extends Component {
+    
+    componentWillMount(){
+        debugger
+        setTimeout(() => {(window).gapi.signin2.render('my-signin2', {
+            'scope': 'profile email',
+            'width': 240,
+            'height': 50,
+            'longtitle': true,
+            'theme': 'dark',
+            'onsuccess': (data) => { this.onSuccess(data); },
+            'onfailure': this.onFailure
+          })}, 1000);
+    }
+
+    onSuccess(data){
+     console.log('google sign in ', data);
+    }
+
+    onFailure(){
+     console.log('google sign in error ');
+        
+    }
+    
     render(){
         return(
-            <div>
-                This is User
-            </div>
+            <div id="my-signin2" data-onsuccess="onSignIn"></div>
         )
     }
 }
