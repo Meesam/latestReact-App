24367f7cb2da8f1d2d1e316ee39bc65d7cd36056
Meesam Z <13222@IIPL.COM>
2018-02-06 18:59:14 +0530

    Include theme , completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {Route,HashRouter as Router , Switch   } from "react-router-dom";
+import LoginLayout from './layouts/loginLayout';
+import MainLayout from './layouts/mainLayout';
 import Home from './components/home';
 import Users from './components/Users';
 import Login from './components/login';
-import LoginLayout from './layouts/loginLayout';
-import MainLayout from './layouts/mainLayout';
+import Dashboard from './components/dashboard';
+
 
 const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
   <Route {...rest} render={props => (
@@ -19,7 +21,8 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <AppRoute exact path="/" layout={MainLayout} component={Home} />
+          <AppRoute exact path="/" layout={MainLayout} component={Dashboard} />
+          <AppRoute exact path="/dashboard" layout={MainLayout} component={Dashboard} />
           <AppRoute exact path="/home" layout={MainLayout} component={Home} />
           <AppRoute exact path="/users" layout={MainLayout} component={Users} />
           <AppRoute exact path="/login" layout={LoginLayout} component={Login} />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import $ from 'jquery';
-import {jQuery}  from 'jquery-easing';
 import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/font-awesome/css/font-awesome.min.css';
+import './vendor/datatables/dataTables.bootstrap4.css';
 import './css/sb-admin.css';
+
 
 ReactDOM.render(
   <App /> , document.getElementById('root'))
