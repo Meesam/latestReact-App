60088680a084d83c444d5f23e0dec1510ac8083f
Meesam Z <13222@IIPL.COM>
2018-02-20 19:15:05 +0530

    no change

--- src/commonComponent/elementContainer.js
+++ src/commonComponent/elementContainer.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 class ElementContainer extends Component{
   constructor(props,context){
@@ -7,25 +8,46 @@ class ElementContainer extends Component{
       Title:this.props.title ? this.props.title :'Container',
       Icon:this.props.icon ? this.props.icon :'fa fa-edit'
     }
+    this.handleClick=this.handleClick.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-     if(this.props.title !== nextProps.title) {
-        this.setState({Title: nextProps.title});
-     }
+    if(this.props.title !== nextProps.title) {
+      this.setState({Title: nextProps.title});
+    }
+  }
+
+  handleClick(e, data) {
+    console.log(data);
   }
 
   render(){ 
     return(
-        <div className="card mb-3 cardContainer">
-         <div className="card-header cardHeader">
-           <i className={this.state.Icon}></i>{this.state.Title}</div>
-            <div className="list-group list-group-flush small">
-              <div className="list-group-item list-group-item-action">
-                  {this.props.children}
-              </div>
+      <div>  
+        <ContextMenuTrigger id="some_unique_identifier">
+          <div className="card mb-3 cardContainer">
+            <div className="card-header cardHeader">
+              <i className={this.state.Icon}></i>{this.state.Title}</div>
+                <div className="list-group list-group-flush small">
+                  <div className="list-group-item list-group-item-action">
+                      {this.props.children}
+                  </div>
+                </div>
             </div>
-        </div>
+          </ContextMenuTrigger>
+          <ContextMenu id="some_unique_identifier">
+            <MenuItem data={{ item: 'item 1' }} onClick={this.handleClick}>
+              ContextMenu Item 1
+            </MenuItem>
+            <MenuItem data={{ item: 'item 2' }} onClick={this.handleClick}>
+              ContextMenu Item 2
+            </MenuItem>
+            <MenuItem divider />
+            <MenuItem data={{ item: 'item 3' }} onClick={this.handleClick}>
+              ContextMenu Item 3
+            </MenuItem>
+        </ContextMenu>
+       </div>
       )
   }
 }
--- src/util/editor.js
+++ src/util/editor.js
@@ -10,23 +10,11 @@ class Editor extends Component{
                 <li className="nav-item">
                     <a className="nav-link active" data-toggle="tab" href="#home" role="tab">Home</a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" data-toggle="tab" href="#profile" role="tab">Profile</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" data-toggle="tab" href="#messages" role="tab">Messages</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" data-toggle="tab" href="#settings" role="tab">Settings</a>
-                </li>
-            </ul>
+            </ul>   
             <div className="tab-content">
                 <div className="tab-pane active" id="home" role="tabpanel">
                     <InputTextDraggable />
                 </div>
-                <div className="tab-pane" id="profile" role="tabpanel">...</div>
-                <div className="tab-pane" id="messages" role="tabpanel">...</div>
-                <div className="tab-pane" id="settings" role="tabpanel">...</div>
             </div>
             
          </div>
